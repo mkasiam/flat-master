f04a4b89c0b755d2ef29861d9990f2159a582f24
mkasiam <kawsarahmmed10001@gmail.com>
2023-11-27 17:53:57 +0600

    not found page addded

--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "../pages/Home/Home/Home";
 import SignUp from "../pages/SignUp/SignUp";
 import Login from "../pages/LogIn/LogIn";
 import Apartment from "../pages/Apartment/Apartment/Apartment";
+import NotFound from "../pages/NotFound/NotFound";
 
 export const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ export const router = createBrowserRouter([
       {
         path: "/login",
         element: <Login></Login>,
+      },
+      {
+        path: "*",
+        element: <NotFound></NotFound>,
       },
     ],
   },
