fe644c70b4e000cd7f81eb6452f6213be82fbf98
mkasiam <kawsarahmmed10001@gmail.com>
2023-11-30 06:44:59 +0600

    axios public and axios secure check and hosted to firebase

--- src/components/routes/Routes.jsx
+++ src/components/routes/Routes.jsx
@@ -1,23 +1,23 @@
 import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "../layout/Dashboard";
 import Main from "../layout/Main";
 import Apartments from "../pages/Apartments/Apartments/Apartments";
+import AdminProfile from "../pages/Dashboard/Admin/AdminProfile/AdminProfile.jsx";
+import AgreementRequest from "../pages/Dashboard/Admin/AgreementRequest/AgreementRequest.jsx";
+import MakeAnnouncement from "../pages/Dashboard/Admin/MakeAnnouncement/MakeAnnouncement.jsx";
+import ManageCoupons from "../pages/Dashboard/Admin/ManageCoupons/ManageCoupons.jsx";
+import ManageMembers from "../pages/Dashboard/Admin/ManageMembers/ManageMembers.jsx";
+import MakePayment from "../pages/Dashboard/Member/MakePayment/MakePayment.jsx";
+import MemberProfile from "../pages/Dashboard/Member/MemberProfile/MemberProfile.jsx";
+import PaymentHistory from "../pages/Dashboard/Member/PaymentHistory/PaymentHistory.jsx";
+import UserProfile from "../pages/Dashboard/User/UserProfile/UserProfile.jsx";
+import Announcements from "../pages/Dashboard/shared/Announcements/Announcements.jsx";
 import Home from "../pages/Home/Home/Home";
 import Login from "../pages/LogIn/LogIn";
 import NotFound from "../pages/NotFound/NotFound";
 import SignUp from "../pages/SignUp/SignUp";
-import PrivateRoute from "./PrivateRoute.jsx";
-import Dashboard from "../layout/Dashboard";
-import UserProfile from "../pages/Dashboard/User/UserProfile/UserProfile.jsx";
-import MemberProfile from "../pages/Dashboard/Member/MemberProfile/MemberProfile.jsx";
-import MakePayment from "../pages/Dashboard/Member/MakePayment/MakePayment.jsx";
-import PaymentHistory from "../pages/Dashboard/Member/PaymentHistory/PaymentHistory.jsx";
 import AdminRoute from "./AdminRoute.jsx";
-import AdminProfile from "../pages/Dashboard/Admin/AdminProfile/AdminProfile.jsx";
-import ManageMembers from "../pages/Dashboard/Admin/ManageMembers/ManageMembers.jsx";
-import MakeAnnouncement from "../pages/Dashboard/Admin/MakeAnnouncement/MakeAnnouncement.jsx";
-import ManageCoupons from "../pages/Dashboard/Admin/ManageCoupons/ManageCoupons.jsx";
-import AgreementRequest from "../pages/Dashboard/Admin/AgreementRequest/AgreementRequest.jsx";
-import Announcements from "../pages/Dashboard/shared/Announcements/Announcements.jsx";
+import PrivateRoute from "./PrivateRoute.jsx";
 
 export const router = createBrowserRouter([
   {
@@ -31,7 +31,7 @@ export const router = createBrowserRouter([
       {
         path: "/apartments",
         element: <Apartments></Apartments>,
-        loader: () => fetch("http://localhost:5000/apartmentCount"),
+        loader: () => fetch("https://flat-master-server.vercel.app/apartmentCount"),
       },
       {
         path: "/signUp",
